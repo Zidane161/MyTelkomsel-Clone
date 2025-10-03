1be6c35cfa5f5422a7da8c006d61f27128ee0329
Zidane161 <muhzidane1618@gmail.com>
2025-10-03 14:42:11 +0700

    "Manage Pulsa and Kuota"

---
+++ src/redux/pulsaslice.tsx
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface PulsaState {
+  pulsa: number;
+  kuota: number;
+  masaAktif: number;
+}
+
+const initialState: PulsaState = {
+  pulsa: 0,
+  kuota: 0,
+  masaAktif: 7, // default 7 hari
+};
+
+const pulsaSlice = createSlice({
+  name: "pulsa",
+  initialState,
+  reducers: {
+    isiPulsa: (state, action: PayloadAction<number>) => {
+      state.pulsa += action.payload;
+    },
+    tambahKuota: (state, action: PayloadAction<number>) => {
+      state.kuota += action.payload;
+    },
+    kurangiMasaAktif: (state, action: PayloadAction<number>) => {
+      if (state.masaAktif > 0) {
+        state.masaAktif -= action.payload;
+      }
+    },
+    resetPulsa: (state) => {
+      state.pulsa = 0;
+      state.kuota = 0;
+      state.masaAktif = 0;
+    },
+  },
+});
+
+export const { isiPulsa, tambahKuota, kurangiMasaAktif, resetPulsa } =
+  pulsaSlice.actions;
+
+export default pulsaSlice.reducer;
